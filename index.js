3fe80a0ef7fe3ccf02e5a0b4741c8e6c8631992b
luna <spotisepic@outlook.com>
2022-11-26 10:58:20 -0600

    replaced garbage fire

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var https = require('https');
 const express = require('express');
 const app = express();
 
-var privateKey = fs.readFileSync('sslcert/private.key.pem', 'utf8');
-var certificate = fs.readFileSync('sslcert/domain.cert.pem', 'utf8');
+// var privateKey = fs.readFileSync('sslcert/private.key.pem', 'utf8');
+// var certificate = fs.readFileSync('sslcert/domain.cert.pem', 'utf8');
 
-var credentials = {key: privateKey, cert: certificate};
+var credentials /*= {key: privateKey, cert: certificate};*/
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(credentials, app);
 
